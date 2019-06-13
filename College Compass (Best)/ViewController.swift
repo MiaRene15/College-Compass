ae136a27f89e0abf5980daa4dba6e1862c36f5be
Apple <apple@Apples-MacBook-Pro.local>
2019-06-13 17:18:33 -0500

    Homepage included

--- College Compass (Best)/ViewController.swift
+++ College Compass (Best)/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
     @IBOutlet weak var titleLabel: UILabel!
     
     @IBOutlet weak var descLabel: UILabel!
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
